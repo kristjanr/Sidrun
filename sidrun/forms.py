0841622d158d4be23bf549274805154c65d85df9
kristjanr <kristjan.r@gmail.com>
2014-07-27 16:29:53 +0300

    Correctly counting the characters when validating the length of task fulfillment input fields.

--- sidrun/forms.py
+++ sidrun/forms.py
@@ -3,6 +3,7 @@ import re
 from django.core.exceptions import ValidationError
 from django.utils.encoding import force_text
 from django.forms.models import BaseInlineFormSet
+from django.utils.html import strip_tags
 
 
 class CustomInlineFormSet(BaseInlineFormSet):
@@ -28,16 +29,22 @@ class CustomInlineFormSet(BaseInlineFormSet):
                 summary_pitch = form.cleaned_data['summary_pitch']
                 validation_errors = []
                 min_summary_length = 140
-                if len(summary_pitch) < min_summary_length:
-                    validation_errors.append(ValidationError("Summary pitch length needs to be at least %d characters. You have %d." % (min_summary_length, len(summary_pitch))))
+                summary_length = len(strip_tags(summary_pitch))
+                if summary_length < min_summary_length:
+                    validation_errors.append(ValidationError("Summary pitch length needs to be at least %d characters. You have %d." % (min_summary_length,
+                                                                                                                                        summary_length)))
                 body = form.cleaned_data['body']
                 min_body_length = 280
-                if len(body) < min_body_length:
-                    validation_errors.append(ValidationError("Body length needs to be at least %d characters. You have %d." % (min_body_length, len(body))))
+                body_length = len(strip_tags(body))
+                if body_length < min_body_length:
+                    validation_errors.append(ValidationError("Body length needs to be at least %d characters. You have %d." % (min_body_length,
+                                                                                                                               body_length)))
                 conclusion = form.cleaned_data['conclusion']
                 min_conclusion_length = 140
-                if len(conclusion) < min_conclusion_length:
-                    validation_errors.append(ValidationError("Conclusion length needs to be at least %d characters. You have %d." % (min_conclusion_length, len(conclusion))))
+                conclusion_length = len(strip_tags(conclusion))
+                if conclusion_length < min_conclusion_length:
+                    validation_errors.append(ValidationError("Conclusion length needs to be at least %d characters. You have %d." % (min_conclusion_length,
+                                                                                                                                     conclusion_length)))
                 references = form.cleaned_data['references']
                 if not self.regex.search(force_text(references)):
                     validation_errors.append(ValidationError("Reference needs to be a valid URL address."))
