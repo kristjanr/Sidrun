fd1ef00fecef3d421918fa93f6377dd433b03197
kristjanr <kristjan.r@gmail.com>
2014-08-20 22:30:56 +0300

    robots and favicon

--- tasks/settings.py
+++ tasks/settings.py
@@ -13,7 +13,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-MEDIA_ROOT = BASE_DIR + "/uploaded_files/"
+MEDIA_ROOT = BASE_DIR + "/media/"
 MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
