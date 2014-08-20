158466042b34cebea0d099f2e6976759fda14b05
kristjanr <kristjan.r@gmail.com>
2014-08-20 22:42:36 +0300

    fix js bug

--- sidrun/static/js/countdown.js
+++ sidrun/static/js/countdown.js
@@ -1,28 +1,34 @@
-var node_containing_countdown = document.getElementById("countdown").nextSibling;
-var countown_text = node_containing_countdown.nodeValue;
-var time_left_splitted = countown_text.split(":");
-var hours = time_left_splitted[0];
-var minutes = time_left_splitted[1];
-var seconds = time_left_splitted[2];
-var milliseconds_left = hours * 3600 * 1000 + minutes * 60 * 1000 + seconds * 1000;
-var target_date = new Date().getTime() + milliseconds_left;
+var countdown = document.getElementById("countdown")
+if (countdown) {
+    var node_containing_countdown = countdown.nextSibling;
+    var countown_text = node_containing_countdown.nodeValue;
+    var time_left_splitted = countown_text.split(":");
+    var hours = time_left_splitted[0];
+    var minutes = time_left_splitted[1];
+    var seconds = time_left_splitted[2];
+    var milliseconds_left = hours * 3600 * 1000 + minutes * 60 * 1000 + seconds * 1000;
+    var target_date = new Date().getTime() + milliseconds_left;
 
-function myTimer() {
-    // variables for time units
-    var days, hours, minutes, seconds;
-    // find the amount of "seconds" between now and target
-    var current_date = new Date().getTime();
-    var seconds_left = (target_date - current_date) / 1000;
+    function myTimer() {
+        // variables for time units
+        var days, hours, minutes, seconds;
+        // find the amount of "seconds" between now and target
+        var current_date = new Date().getTime();
+        var seconds_left = (target_date - current_date) / 1000;
 
-    // do some time calculations
+        // do some time calculations
 
-    hours = parseInt(seconds_left / 3600);
-    seconds_left = seconds_left % 3600;
+        hours = parseInt(seconds_left / 3600);
+        seconds_left = seconds_left % 3600;
 
-    minutes = parseInt(seconds_left / 60);
-    seconds = parseInt(seconds_left % 60);
+        minutes = parseInt(seconds_left / 60);
+        seconds = parseInt(seconds_left % 60);
 
-    node_containing_countdown.nodeValue = hours + ":" + minutes + ":" + seconds;
+        node_containing_countdown.nodeValue = hours + ":" + minutes + ":" + seconds;
 
+    }
+
+    var myVar = setInterval(function () {
+        myTimer()
+    }, 1000);
 }
-var myVar = setInterval(function(){myTimer()}, 1000);
